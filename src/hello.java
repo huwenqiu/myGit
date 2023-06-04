8f5b9c7804b28b71218c7fdea674516ffb64a88e
hwq <asd@qq.com>
2023-06-04 10:44:49 +0800

    third commit

--- src/hello.java
+++ src/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello world v2");
         System.out.println("hello world v3");
+        System.out.println("hello world v4 hot-fix");
     }
 }
